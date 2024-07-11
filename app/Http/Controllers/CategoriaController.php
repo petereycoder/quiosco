7145e1f1839bd574fabf49c4ab5bda564f7771aa
petereycoder <fp.pipe.reyes@gmail.com>
2024-07-11 10:36:25 -0600

    Se creó una API Resource para Categorias

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\CategoriaCollection;
 use App\Models\Categoria;
 use Illuminate\Http\Request;
 
@@ -9,6 +10,7 @@ class CategoriaController extends Controller
 {
     //
     public function index(){
-        return response()->json(['categorias' => Categoria::all()]);
+        //return response()->json(['categorias' => Categoria::all()]);
+        return new CategoriaCollection(Categoria::all());
     }
 }
