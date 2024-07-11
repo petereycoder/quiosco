f658c9e8ece4890811cf660f9df535bc1b280f46
petereycoder <fp.pipe.reyes@gmail.com>
2024-07-11 10:25:15 -0600

    Se creó una respuesta json para las categorias

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Categoria;
 use Illuminate\Http\Request;
 
 class CategoriaController extends Controller
 {
     //
+    public function index(){
+        return response()->json(['categorias' => Categoria::all()]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoriaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/categorias', [CategoriaController::class, 'index']);
