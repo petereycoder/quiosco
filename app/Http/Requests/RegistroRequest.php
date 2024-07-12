6d51ae668c0a60947becfeb0ccb23bc2a018ce37
petereycoder <fp.pipe.reyes@gmail.com>
2024-07-12 09:42:36 -0600

    Se finalizó las reglas de validación y se añadió el endpoint

--- app/Http/Requests/RegistroRequest.php
+++ app/Http/Requests/RegistroRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password as PasswordRules;
 
 class RegistroRequest extends FormRequest
 {
@@ -23,6 +24,12 @@ class RegistroRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string'],
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => [
+                'required',
+                'confirmed',
+                PasswordRules::min(8)->letters()->symbols()->numbers()
+            ]
         ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\ProductoController;
 use Illuminate\Http\Request;
@@ -11,3 +12,7 @@ Route::get('/user', function (Request $request) {
 
 Route::apiResource('/categorias', CategoriaController::class);
 Route::apiResource('/productos', ProductoController::class);
+
+//Autenticación
+Route::post('/registro', [AuthController::class, 'register']);
+
